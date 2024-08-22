f6d71b33e4a7b2dfbb16bc5a06461422a15ad4bd
Dima Usov <dmitry.usov@yahoo.com>
2024-08-22 13:08:43 +0400

    refactor: add toast to test page

--- src/app/components/backend-test/backend-test.component.ts
+++ src/app/components/backend-test/backend-test.component.ts
@@ -4,8 +4,10 @@ import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { FieldsetModule } from 'primeng/fieldset';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { RippleModule } from 'primeng/ripple';
 
 import { BackendService } from '../../services/backend.service';
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'app-backend-test',
@@ -16,14 +18,38 @@ import { BackendService } from '../../services/backend.service';
     FieldsetModule,
     ButtonModule,
     InputTextModule,
+    RippleModule,
   ],
   templateUrl: './backend-test.component.html',
   styleUrl: './backend-test.component.scss',
 })
 export class BackendTestComponent {
-  constructor(private backendService: BackendService) {}
+  constructor(
+    private backendService: BackendService,
+    private toastService: ToastService,
+  ) {}
 
   onGet(endpoint: string) {
     this.backendService.logGETResponse(endpoint);
+  }
+
+  showSuccess() {
+    this.toastService.success('Congratulations!');
+  }
+
+  showInfo() {
+    this.toastService.info(
+      'Please see attached document for your review and approval.',
+    );
+  }
+
+  showWarn() {
+    this.toastService.warn(
+      'Hurricane conditions (sustained winds of 74 mph or greater) are expected somewhere within the specified area',
+    );
+  }
+
+  showError() {
+    this.toastService.error(' A deviation from accuracy or correctness!');
   }
 }
