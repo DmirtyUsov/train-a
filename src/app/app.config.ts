c7898252400a4fdbc505d0c4fa1fbd9b73f1f0b8
Dima <dmitry.usov@yahoo.com>
2024-08-23 19:24:39 +0300

    Feat: TRA-28 Notification Service (#12)
    * feat: add ToastService
    
    * refactor: add toast to test page
    
    * refactor: add link to test page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 import { routes } from './app.routes';
 import { appStoreProviders } from './app.store';
 
@@ -12,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
+    MessageService,
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
+import { BackendTestComponent } from './components/backend-test/backend-test.component';
 import { SigninPageComponent } from './pages/signin-page/signin-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
+  },
+  {
+    path: 'test',
+    component: BackendTestComponent,
   },
   {
     path: 'signin',
