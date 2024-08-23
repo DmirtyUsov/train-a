467e37011e22e01906ef303c02aa0b93a38397ad
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-08-23 19:12:14 +0300

    Feat: TRA-24 sign in page (#13)
    * feat: add sign in form
    
    * feat: add sign in page
    
    * feat: add validation
    
    * fix: fix email validation
    
    * fix: fix email validation
    
    * fix: clean up code
    
    * fix: clean up code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
+import { SigninPageComponent } from './pages/signin-page/signin-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
+  },
+  {
+    path: 'signin',
+    component: SigninPageComponent,
   },
 ];
---
+++ src/app/components/sign-in-form/sign-in-form.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { FieldsetModule } from 'primeng/fieldset';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
+@Component({
+  selector: 'app-sign-in-form',
+  standalone: true,
+  imports: [
+    InputGroupModule,
+    InputGroupAddonModule,
+    FieldsetModule,
+    ButtonModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    PasswordModule,
+  ],
+  templateUrl: './sign-in-form.component.html',
+  styleUrl: './sign-in-form.component.scss',
+})
+export class SignInFormComponent implements OnInit {
+  public signInForm!: FormGroup;
+
+  public isSubmitted: boolean = false;
+
+  constructor(private formBuilder: NonNullableFormBuilder) {}
+
+  ngOnInit(): void {
+    this.signInForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[\w\d_]+@[\w\d_]+.\w{2,7}$/),
+        ],
+      ],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSignIn(): void {
+    // const { email, password } = this.signInForm.value;
+    this.isSubmitted = true;
+  }
+
+  get email() {
+    return this.signInForm.get('email');
+  }
+
+  get password() {
+    return this.signInForm.get('password');
+  }
+}
