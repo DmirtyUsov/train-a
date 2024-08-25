b26f544132ef899665246503d3bbda4c208c9751
Dima Usov <dmitry.usov@yahoo.com>
2024-08-25 18:35:42 +0400

    chore: add NRGX DevTools Providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { MessageService } from 'primeng/api';
 import { routes } from './app.routes';
 import { appStoreProviders } from './app.store';
@@ -14,5 +19,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideHttpClient(),
     MessageService,
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };
