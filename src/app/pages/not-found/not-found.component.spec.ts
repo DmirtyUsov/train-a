9b9e998c4b45af42a0ead9bb2259a430bb4d0082
Dima Usov <dmitry.usov@yahoo.com>
2024-08-27 19:20:50 +0400

    feat: add not found page

--- src/app/pages/not-found/not-found.component.spec.ts
+++ src/app/pages/not-found/not-found.component.spec.ts
@@ -8,8 +8,9 @@ describe('NotFoundComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NotFoundComponent],
-    }).compileComponents();
+      imports: [NotFoundComponent]
+    })
+    .compileComponents();
 
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
