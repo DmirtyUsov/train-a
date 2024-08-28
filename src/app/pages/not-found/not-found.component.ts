17550ef52c419deb40772540877da915197796ec
Dima <dmitry.usov@yahoo.com>
2024-08-28 22:48:14 +0300

    Feat: TRA-15 Header (#21)
    * feat: add save/restore token from localStorage
    
    * feat: add not found page
    
    * feat: add not authorized page
    
    * refactor: header
    
    * fix: after rebase
    
    * fix: location back

---
+++ src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,17 @@
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss',
+})
+export class NotFoundComponent {
+  constructor(private location: Location) {}
+
+  navigateBack(): void {
+    this.location.back();
+  }
+}
