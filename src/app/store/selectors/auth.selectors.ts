c9092ba21c34260551183e8e8f97237231b64520
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-08-28 12:33:13 +0300

    refactor: refactor feature selector

--- src/app/store/selectors/auth.selectors.ts
+++ src/app/store/selectors/auth.selectors.ts
@@ -1,8 +1,5 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-// import { AppState } from '../state.model';
 import { AuthState } from '../models/auth-state.model';
-
-// const selectFeature = (state: AppState) => state.auth;
 
 const selectFeature = createFeatureSelector<AuthState>('auth');
 
