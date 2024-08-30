733b70a2d4bdd44bb132e0b95c67e35df0e61524
Dmitry.Rukovichnikov <57582285+Dmitruk89@users.noreply.github.com>
2024-08-30 11:03:29 +0400

    Feat/tra 36 home page search form (#25)
    * feat: add user model
    
    * feat: format to meet linter requirements
    
    * feat: add station model
    
    * feat: add order model
    
    * feat: add ride model
    
    * feat: add route model
    
    * Models (#6)
    
    * feat: add user model
    
    * feat: format to meet linter requirements
    
    * feat: add station model
    
    * feat: add order model
    
    * feat: add ride model
    
    * feat: add route model
    
    * feat: add search and stations services
    
    * feat: add ngrx, select stations from store for search dropdowns
    
    * feat: connect selected cities with search state, set headers using state values
    
    * tryfix some linting errors
    
    * tryfix some linting errors
    
    * tryfix some linting errors
    
    * tryfix all linting errors
    
    * tryfix lint errors
    
    * feat: clean up some code
    
    * Feat/tra 12 search service (#7)
    
    * feat: add user model
    
    * feat: format to meet linter requirements
    
    * feat: add station model
    
    * feat: add order model
    
    * feat: add ride model
    
    * feat: add route model
    
    * feat: add search and stations services
    
    * feat: add ngrx, select stations from store for search dropdowns
    
    * feat: connect selected cities with search state, set headers using state values
    
    * tryfix some linting errors
    
    * tryfix some linting errors
    
    * tryfix some linting errors
    
    * tryfix all linting errors
    
    * tryfix lint errors
    
    * feat: clean up some code
    
    * feat: display search result on template
    
    * feat: add initial template to search result
    
    * tryfix lint errors
    
    * feat: crate a search-result item component
    
    * feat: display data for search result item
    
    * tryfix linter errors
    
    * feat: move utils methods to helpers
    
    * fix linter errors
    
    * feat: add all the nessecery data to search page
    
    * feat: create helpers function to transform data
    
    * fix some linting error
    
    * trifix linting errors
    
    * tryfix linting errors
    
    * tryfix linting errors
    
    * tryfix linting errors
    
    * feat: from and to cities input are now autocomplete dropdowns
    
    * feat: search button is disabled until valid
    
    * add placeholder for date field, add margins between fields
    
    * fix conflicts

--- src/app/pages/home-page/components/search-form/search-form.component.ts
+++ src/app/pages/home-page/components/search-form/search-form.component.ts
@@ -1,32 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Observable } from 'rxjs';
-import { filter, map, tap } from 'rxjs/operators';
-import { DropdownModule } from 'primeng/dropdown';
+import { map, filter, tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { CommonModule } from '@angular/common';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 import { CalendarModule } from 'primeng/calendar';
 import { ButtonModule } from 'primeng/button';
-import { CommonModule } from '@angular/common';
-import { SearchResponseStation } from '../../../../models/station.models';
-import {
-  selectFromStationSelected,
-  selectMappedDate,
-  selectToStationSelected,
-} from '../../../../store/selectors/search.selectors';
 import { loadStations } from '../../../../store/actions/stations.actions';
 import { selectTransformedStations } from '../../../../store/selectors/stations.selectors';
-import { loadSearchResults } from '../../../../store/actions/search-result.actions';
+import { SearchResponseStation } from '../../../../models/station.models';
 import {
   selectDate,
   selectFromStation,
   selectToStation,
 } from '../../../../store/actions/search.actions';
+import {
+  selectFromStationSelected,
+  selectMappedDate,
+  selectToStationSelected,
+} from '../../../../store/selectors/search.selectors';
+import { loadSearchResults } from '../../../../store/actions/search-result.actions';
 
 @Component({
   selector: 'app-search-form',
   standalone: true,
   imports: [
-    DropdownModule,
+    AutoCompleteModule,
     ReactiveFormsModule,
     CalendarModule,
     ButtonModule,
@@ -44,26 +49,33 @@ export class SearchFormComponent implements OnInit {
 
   toStation$: Observable<SearchResponseStation | null>;
 
+  date$: Observable<Date | null>;
+
   fromStations: SearchResponseStation[] = [];
 
   toStations: SearchResponseStation[] = [];
 
-  date$: Observable<Date | null>;
+  filteredFromStations: SearchResponseStation[] = [];
+
+  filteredToStations: SearchResponseStation[] = [];
+
+  minDate: Date;
 
   constructor(
     private fb: FormBuilder,
     private store: Store,
   ) {
     this.formGroup = this.fb.group({
-      selectedFromStation: [null],
-      selectedToStation: [null],
-      date: [null],
+      selectedFromStation: [null, Validators.required],
+      selectedToStation: [null, Validators.required],
+      date: [null, Validators.required],
     });
 
     this.allStations$ = this.store.select(selectTransformedStations);
     this.fromStation$ = this.store.select(selectFromStationSelected);
     this.toStation$ = this.store.select(selectToStationSelected);
     this.date$ = this.store.select(selectMappedDate);
+    this.minDate = new Date();
 
     this.allStations$
       .pipe(
@@ -71,6 +83,8 @@ export class SearchFormComponent implements OnInit {
         map((stations) => {
           this.fromStations = stations;
           this.toStations = stations;
+          this.filteredFromStations = stations;
+          this.filteredToStations = stations;
         }),
       )
       .subscribe();
@@ -79,25 +93,31 @@ export class SearchFormComponent implements OnInit {
   ngOnInit(): void {
     this.store.dispatch(loadStations());
 
-    this.fromStation$.pipe(
-      tap((selectedStation) => {
-        this.formGroup.patchValue({ selectedFromStation: selectedStation });
-      }),
-    );
+    this.fromStation$
+      .pipe(
+        tap((selectedStation) => {
+          this.formGroup.patchValue({ selectedFromStation: selectedStation });
+        }),
+      )
+      .subscribe();
 
-    this.toStation$.pipe(
-      tap((selectedStation) => {
-        this.formGroup.patchValue({ selectedToStation: selectedStation });
-      }),
-    );
+    this.toStation$
+      .pipe(
+        tap((selectedStation) => {
+          this.formGroup.patchValue({ selectedToStation: selectedStation });
+        }),
+      )
+      .subscribe();
 
-    this.date$.pipe(
-      tap((date) => {
-        if (date) {
-          this.formGroup.patchValue({ date });
-        }
-      }),
-    );
+    this.date$
+      .pipe(
+        tap((date) => {
+          if (date) {
+            this.formGroup.patchValue({ date });
+          }
+        }),
+      )
+      .subscribe();
   }
 
   onSubmit(): void {
@@ -135,5 +155,17 @@ export class SearchFormComponent implements OnInit {
     if (event) {
       this.store.dispatch(selectDate({ date: isoDate }));
     }
+  }
+
+  filterFromStation(event: { query: string }): void {
+    this.filteredFromStations = this.fromStations.filter((station) =>
+      station.city.toLowerCase().includes(event.query.toLowerCase()),
+    );
+  }
+
+  filterToStation(event: { query: string }): void {
+    this.filteredToStations = this.toStations.filter((station) =>
+      station.city.toLowerCase().includes(event.query.toLowerCase()),
+    );
   }
 }
