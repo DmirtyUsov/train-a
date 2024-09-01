f9bf9ecefc9719c0ede76759b3df134afecd1998
Dmitry Rukovichnikov <dmitry.rukovichnikov@gmail.com>
2024-09-01 10:56:58 +0400

    feat: add navigate back button

--- src/app/pages/trip-page/components/trip-header/trip-header.component.ts
+++ src/app/pages/trip-page/components/trip-header/trip-header.component.ts
@@ -1,14 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { Ride } from '../../../../models/ride.model';
 import { DatePipe } from '@angular/common';
+import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-trip-header',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, PanelModule, ButtonModule],
   templateUrl: './trip-header.component.html',
   styleUrls: ['./trip-header.component.scss'],
 })
 export class TripHeaderComponent {
   @Input() ride!: Ride;
+
+  constructor(private router: Router) {}
+
+  navigateBack() {
+    this.router.navigate(['']);
+  }
 }
