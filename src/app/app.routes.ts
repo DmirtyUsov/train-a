dc19af1ad5802a9b16c1babdcc241d385f03722d
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-09-01 22:05:37 +0300

    feat: add profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SigninPageComponent } from './pages/signin-page/signin-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { TripPageComponent } from './pages/trip-page/trip-page/trip-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 export const routes: Routes = [
   {
@@ -24,6 +25,10 @@ export const routes: Routes = [
     component: SignupPageComponent,
   },
 
+  {
+    path: 'profile',
+    component: ProfilePageComponent,
+  },
   {
     path: 'admin',
     loadChildren: () =>
---
+++ src/app/components/profile/user-info-block/user-info-block.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-info-block.component.html',
+  styleUrl: './user-info-block.component.scss',
+})
+export class UserInfoBlockComponent {}
