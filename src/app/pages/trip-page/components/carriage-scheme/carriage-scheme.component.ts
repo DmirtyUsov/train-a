2c8dc2ba7092d01ab0a7125349a9a5f9ad8509e6
Dmitry Rukovichnikov <dmitry.rukovichnikov@gmail.com>
2024-09-02 14:24:16 +0400

    feat: build car grid scheme on template

--- src/app/pages/trip-page/components/carriage-scheme/carriage-scheme.component.ts
+++ src/app/pages/trip-page/components/carriage-scheme/carriage-scheme.component.ts
@@ -1,18 +1,59 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DividerModule } from 'primeng/divider';
 import { CarriageType } from '../../../../models/carriage.model';
 
 @Component({
   selector: 'app-carriage-scheme',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DividerModule],
   templateUrl: './carriage-scheme.component.html',
-  styleUrl: './carriage-scheme.component.scss',
+  styleUrls: ['./carriage-scheme.component.scss'],
 })
 export class CarriageSchemeComponent implements OnInit {
   @Input() carriageIndex!: number;
   @Input() carriageInfo!: CarriageType | undefined;
 
+  leftSeatNumbers: number[][] = [];
+  rightSeatNumbers: number[][] = [];
+
   ngOnInit(): void {
-    console.log('carriage', this.carriageInfo);
+    if (this.carriageInfo) {
+      console.log('carriageInfo', this.carriageInfo);
+      this.generateSeatNumbers();
+    }
+  }
+
+  generateSeatNumbers(): void {
+    if (!this.carriageInfo) return;
+
+    const leftSeats = this.carriageInfo.leftSeats;
+    const rightSeats = this.carriageInfo.rightSeats;
+    const rows = this.carriageInfo.rows;
+
+    const totalSeats = (leftSeats + rightSeats) * rows;
+    const seatNumbers = Array.from({ length: totalSeats }, (_, i) => i + 1);
+
+    this.leftSeatNumbers = Array.from({ length: rows }, () => []);
+    this.rightSeatNumbers = Array.from({ length: rows }, () => []);
+
+    let seatIndex = 0;
+
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < leftSeats; col++) {
+        if (seatIndex < seatNumbers.length) {
+          this.leftSeatNumbers[row].push(seatNumbers[seatIndex++]);
+        }
+      }
+
+      for (let col = 0; col < rightSeats; col++) {
+        if (seatIndex < seatNumbers.length) {
+          this.rightSeatNumbers[row].push(seatNumbers[seatIndex++]);
+        }
+      }
+    }
+
+    this.leftSeatNumbers = this.leftSeatNumbers.map((row) => row.reverse());
+    this.rightSeatNumbers = this.rightSeatNumbers.map((row) => row.reverse());
   }
 }
