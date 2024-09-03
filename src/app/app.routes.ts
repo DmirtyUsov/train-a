7ea6b601aaf9ce2811d20fb3a0057d6b40e44f97
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-09-03 21:59:26 +0300

    Feat: TRA-5 router guards (#23)
    * feat: add authenticated user guard
    
    * feat: add manager guard
    
    * refactor: rename guard
    
    * feat: add user guard
    
    * feat: add user guard
    
    * feat: add redirects
    
    * feat: add canMatch guard
    
    * feat: redirection to home
    
    * fix: guard now return urlTree
    
    * refactor: remove child components
    
    * feat: add guards to routes
    
    * feat: add order page route
    
    * refactor: remove unnecessary paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
-import { BackendTestComponent } from './components/backend-test/backend-test.component';
 import { SigninPageComponent } from './pages/signin-page/signin-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { TripPageComponent } from './pages/trip-page/trip-page/trip-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
-import { managerGuard } from './guards/manager.guard';
 import { NotAuthorizedComponent } from './pages/not-authorized/not-authorized.component';
+import { guestGuard } from './guards/guest.guard';
+import { managerGuard } from './guards/manager.guard';
+import { userGuard } from './guards/user.guard';
+import { OrderPageComponent } from './pages/order-page/order-page.component';
 
 export const routes: Routes = [
   {
@@ -15,21 +17,14 @@ export const routes: Routes = [
     component: HomePageComponent,
   },
   {
-    path: 'test',
-    component: BackendTestComponent,
-  },
-  {
     path: 'signin',
     component: SigninPageComponent,
+    canActivate: [guestGuard],
   },
   {
     path: 'signup',
     component: SignupPageComponent,
-  },
-
-  {
-    path: 'profile',
-    component: ProfilePageComponent,
+    canActivate: [guestGuard],
   },
   {
     path: 'admin',
@@ -41,18 +36,17 @@ export const routes: Routes = [
   {
     path: 'admin',
     component: NotAuthorizedComponent,
+    children: [{ path: '**', component: NotAuthorizedComponent }],
   },
   {
-    path: 'admin/stations',
-    component: NotAuthorizedComponent,
+    path: 'profile',
+    component: ProfilePageComponent,
+    canActivate: [userGuard],
   },
   {
-    path: 'admin/carriages',
-    component: NotAuthorizedComponent,
-  },
-  {
-    path: 'admin/routes',
-    component: NotAuthorizedComponent,
+    path: 'orders',
+    component: OrderPageComponent,
+    canActivate: [userGuard],
   },
   {
     path: 'trip/:rideId',
