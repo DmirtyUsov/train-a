2a8aaedb52dd4c4b35ce56e83ad8c7a5cb7549f6
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-09-03 19:34:55 +0300

    feat: add guards to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
-import { BackendTestComponent } from './components/backend-test/backend-test.component';
 import { SigninPageComponent } from './pages/signin-page/signin-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { TripPageComponent } from './pages/trip-page/trip-page/trip-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
-import { managerGuard } from './guards/manager.guard';
 import { NotAuthorizedComponent } from './pages/not-authorized/not-authorized.component';
 import { guestGuard } from './guards/guest.guard';
 import { managerGuard } from './guards/manager.guard';
+import { userGuard } from './guards/user.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
-  },
-  {
-    path: 'test',
-    component: BackendTestComponent,
   },
   {
     path: 'signin',
@@ -29,16 +24,6 @@ export const routes: Routes = [
     path: 'signup',
     component: SignupPageComponent,
     canActivate: [guestGuard],
-  },
-  {
-    path: 'admin',
-    component: BackendTestComponent,
-    canMatch: [managerGuard],
-  },
-
-  {
-    path: 'profile',
-    component: ProfilePageComponent,
   },
   {
     path: 'admin',
@@ -63,6 +48,11 @@ export const routes: Routes = [
   {
     path: 'admin/routes',
     component: NotAuthorizedComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfilePageComponent,
+    canActivate: [userGuard],
   },
   {
     path: 'trip/:rideId',
